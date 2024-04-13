acfc6dbd16b169b25460dc1e88a94fe6ed6ef43c
Filipe Moura <100590351+filipexcode@users.noreply.github.com>
2024-04-13 11:03:54 -0300

    Add files via upload

---
+++ encapsulamento/Sistema.java
@@ -0,0 +1,10 @@
+package encapsulamento;
+
+public class Sistema {
+    public static void main(String[] args) {
+        ContaCorrente c1 = new ContaCorrente(21);
+        ContaCorrente c2 = new ContaCorrente(32);
+
+
+    }
+}
